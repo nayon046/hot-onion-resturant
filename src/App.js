e1d6f3963c36038d842f8f2fcc79aae0386c69e9
nayon046 <nayon.storerepublic@gmail.com>
2020-04-07 19:42:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { AuthProvider, PrivateRoute } from './Components/SignUp/useAuth';
+import Header from './Components/Header/Header';
+import Banner from './Components/Banner/Banner';
+import Foods from './Components/Foods/Foods';
+import Features from './Components/Features/Features';
+import Footer from './Components/Footer/Footer';
+import FoodDetails from './Components/FoodDetails/FoodDetails';
+import { useState } from 'react';
+import SearchResult from './Components/SearchResult/SearchResult';
+import NotFound from './Components/NotFound/NotFound';
+import SignUp from './Components/SignUp/SignUp';
+import Shipment from './Components/Shipment/Shipment';
+import OrderComplete from './Components/OrderComplete/OrderComplete';
+
 
 function App() {
+
+  const [cart , setCart] = useState([]);
+  const [deliveryDetails , setDeliveryDetails] = useState({
+    todoor:null,road:null, flat:null, businessname:null, address: null
+  });
+  const deliveryDetailsHandler = (data) => {
+      setDeliveryDetails(data)
+  }
+  const clearCart = () => {
+    setCart([])
+  }
+  const cartHandler = (data) => {
+    const alreadyAdded = cart.find(crt => crt.id == data.id );
+    const newCart = [...cart,data]
+    setCart(newCart);
+    if(alreadyAdded){
+      const reamingCarts = cart.filter(crt => cart.id != data);
+      setCart(reamingCarts);
+    }else{
+      const newCart = [...cart,data]
+      setCart(newCart);
+    }
+   
+  }
+
+  const checkOutItemHandler = (productId, productQuantity) => {
+    const newCart = cart.map(item => {
+      if(item.id == productId){
+          item.quantity = productQuantity;
+      }
+      return item;
+    })
+
+    const filteredCart = newCart.filter(item => item.quantity > 0)
+    setCart(filteredCart)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <AuthProvider>
+      <Router>
+        <div className="main">
+
+          <Switch>
+
+            <Route exact path="/">
+              <Header cart={cart}></Header>
+              <Banner></Banner>
+              <Foods cart={cart}></Foods>
+              <Features></Features>
+              <Footer></Footer>
+            </Route>
+
+              <Route path="/food/:id">
+                  <Header cart={cart}></Header>
+                  <FoodDetails cart={cart} cartHandler={cartHandler}></FoodDetails>
+                  <Footer></Footer>
+              </Route>
+              <Route path="/search=:searchQuery">
+                  <Header cart={cart}></Header>
+                  <Banner></Banner>
+                  <SearchResult></SearchResult>
+                  <Features></Features>
+                  <Footer></Footer>
+              </Route>
+              <PrivateRoute path="/checkout">
+                <Header cart={cart}></Header>
+                
+                <Shipment deliveryDetails={deliveryDetails} deliveryDetailsHandler={deliveryDetailsHandler} cart={cart} clearCart={clearCart} checkOutItemHandler={checkOutItemHandler}></Shipment>
+                <Footer></Footer>
+            </PrivateRoute>
+            <PrivateRoute path="/order-complete">
+              <Header cart={cart}></Header>
+              <OrderComplete deliveryDetails={deliveryDetails}></OrderComplete>
+              <Footer></Footer>
+            </PrivateRoute>
+              <Route path="/login">
+                  <SignUp></SignUp>
+              </Route>
+              <Route path="*">
+                  <NotFound></NotFound>
+              </Route>
+          </Switch>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyAHZqH2JaU9wub93Iz4DHNMnW_Do_VtfiM",
+    authDomain: "hot-onion-5b6d7.firebaseapp.com",
+    databaseURL: "https://hot-onion-5b6d7.firebaseio.com",
+    projectId: "hot-onion-5b6d7",
+    storageBucket: "hot-onion-5b6d7.appspot.com",
+    messagingSenderId: "381592548546",
+    appId: "1:381592548546:web:76ad47c759e1e44ddb775b"
+  };
+  export default firebaseConfig;
